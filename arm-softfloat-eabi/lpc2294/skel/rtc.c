d8d5f7a3f07043c583a0d8152b0b86f0bd558a89
daniel <danieruru@gmail.com>
2012-01-31 19:33:03 +0900

    some RTC stuff

---
+++ arm-softfloat-eabi/lpc2294/skel/rtc.c
@@ -0,0 +1,10 @@
+/*
+ * rtc.c
+ *
+ *  Created on: Jan 31, 2012
+ *      Author: daniel
+ */
+
+#include "rtc.h"
+#include "lpc21xx.h"
+
---
+++ arm-softfloat-eabi/lpc2294/skel/rtc.h
@@ -0,0 +1,36 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Jan 31, 2012
+ *      Author: daniel
+ */
+
+#ifndef RTC_H_
+#define RTC_H_
+
+#define RTC_ILR_RTCCIF 1
+#define RTC_ILR_RTCALF (1 << 1)
+
+#define RTC_CCR_CLKEN 1
+#define RTC_CCR_CTCRST (1 << 1)
+#define RTC_CCR_CTTEST (3 << 2)
+
+#define RTC_CIIR_IMSEC	1
+#define RTC_CIIR_IMMIN	(1 << 1)
+#define RTC_CIIR_IMHOUR	(1 << 2)
+#define RTC_CIIR_IMDOM	(1 << 3)
+#define RTC_CIIR_IMDOW	(1 << 4)
+#define RTC_CIIR_IMDOY	(1 << 5)
+#define RTC_CIIR_IMMON	(1 << 6)
+#define RTC_CIIR_IMYEAR (1 << 7)
+
+#define RTC_AMR_AMRSEC	1
+#define RTC_AMT_AMRMIN	(1 << 1)
+#define RTC_AMT_AMRHOUR	(1 << 2)
+#define RTC_AMT_AMRDOM	(1 << 3)
+#define RTC_AMT_AMRDOW	(1 << 4)
+#define RTC_AMT_AMRDOY	(1 << 5)
+#define RTC_AMT_AMRMON	(1 << 6)
+#define RTC_AMT_AMRYEAR	(1 << 7)
+
+#endif /* RTC_H_ */
