da2a557e29480efce556bd159bd4835c031e2dca
daniel <danieruru@gmail.com>
2012-01-31 19:41:33 +0900

    that should be everything

---
+++ arm-softfloat-eabi/lpc2294/skel/pwm.c
@@ -0,0 +1,8 @@
+/*
+ * pwm.c
+ *
+ *  Created on: Jan 31, 2012
+ *      Author: daniel
+ */
+
+#include "pwm.h"
---
+++ arm-softfloat-eabi/lpc2294/skel/pwm.h
@@ -0,0 +1,13 @@
+/*
+ * pwm.h
+ *
+ *  Created on: Jan 31, 2012
+ *      Author: daniel
+ */
+
+#ifndef PWM_H_
+#define PWM_H_
+
+
+
+#endif /* PWM_H_ */
