1d0fb09c36793b4b5df3c539a637144cc1285bae
daniel <danieruru@gmail.com>
2012-01-31 22:00:28 +0900

    WDT code

--- arm-softfloat-eabi/lpc2294/skel/wdt.c
+++ arm-softfloat-eabi/lpc2294/skel/wdt.c
@@ -5,4 +5,26 @@
  *      Author: daniel
  */
 
+#include "wdt.h"
+#include "lpc21xx.h"
+#include <stdbool.h>
 
+#define FEEDFIRST 0xAA
+#define FEEDSECOND 0x55
+
+void wdt_enable(uint32_t constant, bool reset) {
+
+	WDTC = constant;
+
+	if (reset) {
+		WDMOD = WDT_WDMOD_WDEN | WDT_WDMOD_WDRESET;
+	}
+	else {
+		WDMOD = WDT_WDMOD_WDEN;
+	}
+}
+
+void wdt_feed() {
+	WDFEED = FEEDFIRST;
+	WDFEED = FEEDSECOND;
+}
--- arm-softfloat-eabi/lpc2294/skel/wdt.h
+++ arm-softfloat-eabi/lpc2294/skel/wdt.h
@@ -8,6 +8,13 @@
 #ifndef WDT_H_
 #define WDT_H_
 
+#include <stdbool.h>
 
+void wdt_enable(uint32_t constant, bool reset);
+void wdt_feed();
 
+#define WDT_WDMOD_WDEN		1
+#define WDT_WDMOD_WDRESET	(1 << 1)
+#define WDT_WDMOD_WDTOF		(1 << 2)
+#define WDT_WDMOD_WDINT		(1 << 3)
 #endif /* WDT_H_ */
