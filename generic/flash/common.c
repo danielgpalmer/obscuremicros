45785b97d408e1de14f1785482da237343f502eb
daniel <danieruru@gmail.com>
2012-03-23 22:49:30 +0900

    missing file

---
+++ generic/flash/common.c
@@ -0,0 +1,21 @@
+/*
+ * common.c
+ *
+ *  Created on: Mar 19, 2012
+ *      Author: daniel
+ */
+
+#include "common.h"
+#include <stdint.h>
+
+uint16_t cfi_readword(uint8_t address) {
+	return flash_read_word(address * 2);
+}
+
+uint8_t cfi_readbyte(uint8_t address) {
+	return (cfi_readword(address) & 0xFF);
+}
+
+void cfi_writebyte(uint8_t address, uint8_t data) {
+	flash_write_word(address * 2, data);
+}
