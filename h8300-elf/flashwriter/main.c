82252ad108ee16d1a4dd5eb863a4d33c6e1ac222
daniel <danieruru@gmail.com>
2012-03-23 22:00:29 +0900

    just need to implement ymodem and wire up the atmel flash driver once the chips come

--- h8300-elf/flashwriter/main.c
+++ h8300-elf/flashwriter/main.c
@@ -10,6 +10,15 @@
 
 #define ROMSIZE 32768
 
+void ymodemsend(){
+	print("Start your ymodem send now..\n");
+}
+
+void ymodemrecv(){
+	print("Start your ymodem recv now..\n");
+}
+
+
 int main(void)
 {
 
@@ -20,16 +29,48 @@ int main(void)
  	volatile uint8_t* eeprom = (uint8_t*) 0xC00000;
 
 	print("flash write thingy\n");
-	print("press r to read the ROM or press w to write the ROM\n");
-	
-	// read rom
-	for(uint16_t i = 0; i < ROMSIZE; i++){
-		romdata[i] = eeprom[i];
-		print(".");
+
+	while(1){
+	print("press r to read the ROM or press w to write the ROM or q to quit\n");
+	char ch = 0;
+
+	while(ch == 0){ // Spin here.
+		mon_getch(&ch);
 	}
 
-	print("Done\n");
+	switch(ch){
+		case 'r':
+		// read rom
+		print("Reading ROM into memory..");
+		for(uint16_t i = 0; i < ROMSIZE; i++){
+			romdata[i] = eeprom[i];
+		}
+		print("Done\n");
+		ymodemsend();
+		break;
+		
+		case 'w':
+		// write rom there
+		ymodemrecv();
+		print("Writing ROM..");
+        	print("Done\n");
+		break;
 
+		case 'q':
+			return 0;
+
+		default:
+			mon_putch(&ch);
+			print(" - eh?\n");
+			break;
+	}
+
+	while(ch != 0){ // Drain any chars out
+		ch = 0;
+		mon_getch(&ch); 
+	}
+
+	}
 	return 0;
 }
 
