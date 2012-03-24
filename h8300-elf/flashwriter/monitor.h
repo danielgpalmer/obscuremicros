2d012b6c68f00c21e0c2650d8bced76dea5cc0d1
daniel <danieruru@gmail.com>
2012-03-24 11:13:19 +0900

    use links ..

--- h8300-elf/flashwriter/monitor.h
+++ h8300-elf/flashwriter/monitor.h
@@ -1,3 +1,1 @@
-void mon_putch(char* ch) __attribute__((section(".itext")));
-void mon_getch(char* ch) __attribute__((section(".itext")));
-void print(char *str) __attribute__((section(".itext")));
+../h8skel/monitor.h
