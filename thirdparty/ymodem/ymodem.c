2a739b7068d5a738abf354b2bf2e39152c07ed32
daniel <danieruru@gmail.com>
2012-04-07 22:46:30 +0900

    doh

--- thirdparty/ymodem/ymodem.c
+++ thirdparty/ymodem/ymodem.c
@@ -48,7 +48,7 @@ static unsigned long crc32(const unsigned char* buf, unsigned long count) {
 	unsigned long i;
 
 	/* This static table adds 1K */
-	static const unsigned long crc_table[256] = {0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419,
+	static const uint32_t crc_table[256] = {0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419,
 		0x706AF48F, 0xE963A535, 0x9E6495A3, 0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD,
 		0xE7B82D07, 0x90BF1D91, 0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551,
 		0x83D385C7, 0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
@@ -125,7 +125,7 @@ static const char *u32_to_str(uint32_t val) {
 	return &num_str[pos];
 }
 
-static unsigned long str_to_u32(char* str) {
+static uint32_t str_to_u32(char* str) {
 	const char *s = str;
 	unsigned long acc;
 	int c;
